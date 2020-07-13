f921d8f96f73e28cf92ec228b045bf63484f6037
AlxandrV <a.vigneaud@gmail.com>
2020-07-13 18:06:10 +0200

    Add - header and nav link

--- script.js
+++ script.js
@@ -2,3 +2,20 @@
 new fullpage('#fullpage', {
 	// options here
 });
+
+// Function burger active
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', () => {
+	burger.classList.toggle('active');
+	nav.classList.toggle('active');
+});
+
+// Function disable burger
+const disable = document.querySelector('#navLinks');
+
+disable.addEventListener('click', () => {	
+	burger.classList.remove('active');
+	nav.classList.remove('active');
+})
