e4116b043aa23a34b3a958dc97f03c8ef67545c2
AlxandrV <a.vigneaud@gmail.com>
2020-07-19 14:48:15 +0200

    Add -ajax form contact

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ disable.addEventListener('click', () => {
 	burger.classList.remove('active');
 	nav.classList.remove('active');
 });
+
+// Ajax form contact
+const form = document.getElementById('ajaxReq');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    let formData = new FormData(form);
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "contact.php", true);
+    xhr.send(formData);
+    xhr.addEventListener("readystatechange", () => {
+        if (xhr.readyState === 3) {
+            console.log("ça charge");
+        }
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // let datas = JSON.parse(xhr.responseText);
+			// console.log(datas);
+			const mail = document.getElementById('mail');
+			const object = document.getElementById('object');
+			const message = document.getElementById('message');
+			mail.value = '';   
+			object.value = '';        
+			message.value = '';        
+
+			const hidden = document.getElementById('hidden');
+			hidden.style.visibility = 'visible';
+		}
+    });
+});
