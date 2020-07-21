b10fa27e988db10bbc2c790e27a86e94abc49c90
AlxandrV <a.vigneaud@gmail.com>
2020-07-21 11:52:10 +0200

    Update - mail php and add shadow img

--- contact.php
+++ contact.php
@@ -1,13 +1,28 @@
 <?php
 $from = $_POST['mail'];
 
-$to      = 'a.vigneaud@gmail.com';
+$to      = 'test-dfq34jbmc@srv1.mail-tester.com';
 $subject = $_POST['object'];
-$message = $_POST['message'];
-$headers = 'From: ' . $from . "\r\n" .
-    'Reply-To: ' . $from . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$message = '
+    <html>
+        <head>
+            <title>Message de votre portfolio</title>
+        </head>
+        <body>
+            <p> From: <' . $from . '></p>
+            <p>' . $_POST['message'] . '
+        </body>
+    </html>
+    ';
 
-mail($to, $subject, $message, $headers);
+// Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=UTF-8';
+
+// En-têtes additionnels
+$headers[] = 'From: ' . $to;
+var_dump($headers);
+
+mail($to, $subject, $message, implode("\r\n", $headers));
 
 echo json_encode(true);
