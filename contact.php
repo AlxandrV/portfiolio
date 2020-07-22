79403f00947d412e62174a923c6669cea9401e79
AlxandrV <a.vigneaud@gmail.com>
2020-07-22 13:35:12 +0200

    Add - captcha

--- contact.php
+++ contact.php
@@ -1,9 +1,24 @@
-<?php
-$from = $_POST['mail'];
+<?php    	   	
+// Ma clé privée
+$secret = "6LeuNQITAAAAAHwUcbXbyFCUudJKRAjcgNRwlaoE";
+// Paramètre renvoyé par le recaptcha
+$response = $_POST['g-recaptcha-response'];
+// On récupère l'IP de l'utilisateur
+$remoteip = $_SERVER['REMOTE_ADDR'];
 
-$to      = 'test-dfq34jbmc@srv1.mail-tester.com';
-$subject = $_POST['object'];
-$message = '
+$api_url = "https://www.google.com/recaptcha/api/siteverify?secret=" 
+    . $secret
+    . "&response=" . $response
+    . "&remoteip=" . $remoteip ;
+
+$decode = json_decode(file_get_contents($api_url), true);
+
+if ($decode['success'] == true) {
+    $from = $_POST['mail'];
+    
+    $to      = 'test-dfq34jbmc@srv1.mail-tester.com';
+    $subject = $_POST['object'];
+    $message = '
     <html>
         <head>
             <title>Message de votre portfolio</title>
@@ -14,15 +29,20 @@ $message = '
         </body>
     </html>
     ';
+    
+    // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
+    $headers[] = 'MIME-Version: 1.0';
+    $headers[] = 'Content-type: text/html; charset=UTF-8';
+    
+    // En-têtes additionnels
+    $headers[] = 'From: ' . $to;
+    var_dump($headers);
+    
+    mail($to, $subject, $message, implode("\r\n", $headers));
+    
+    echo json_encode(true);
+}
 
-// Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
-$headers[] = 'MIME-Version: 1.0';
-$headers[] = 'Content-type: text/html; charset=UTF-8';
-
-// En-têtes additionnels
-$headers[] = 'From: ' . $to;
-var_dump($headers);
-
-mail($to, $subject, $message, implode("\r\n", $headers));
-
-echo json_encode(true);
+else {
+    // C'est un robot ou le code de vérification est incorrecte
+}    
